c6adb5d6eddc581b5e0ea24cca42f7382b877138
student <student@PC634S16.asost.local>
2015-06-26 14:11:19 +0900

    コミット

---
+++ src/jp/ac/asojuku/jousen/rpg/Battle_Scene.java
@@ -0,0 +1,15 @@
+package jp.ac.asojuku.jousen.rpg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Battle_Scene extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.battle_scene);
+
+
+	}
+}
--- src/jp/ac/asojuku/jousen/rpg/Danjon_Select.java
+++ src/jp/ac/asojuku/jousen/rpg/Danjon_Select.java
@@ -1,7 +1,10 @@
 package jp.ac.asojuku.jousen.rpg;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Danjon_Select extends Activity {
 	@Override
@@ -9,5 +12,16 @@ public class Danjon_Select extends Activity {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.danjon_select);
+
+		Button what_legend = (Button)findViewById(R.id.what_legend);
+		what_legend.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO 自動生成されたメソッド・スタブ
+				Intent what_legend = new Intent(Danjon_Select.this,Battle_Scene.class);
+				startActivity(what_legend);
+			}
+		});
 	}
 }
